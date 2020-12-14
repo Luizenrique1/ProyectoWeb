e3a6064bd48892e391d2db8362b8f0f538cb03f5
Luizenrique1 <48364368+Luizenrique1@users.noreply.github.com>
2020-12-13 23:25:26 -0600

    Update SISession.php

---
+++ Paginas/SISession.php
@@ -0,0 +1,39 @@
+<?php
+    
+    session_start();
+    if($_SESSION['autenticado'] == false) {
+        header("location:../Paginas/NoSession.php");
+    } 
+
+?>
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <meta charset='utf-8'>
+        <title>Login</title>
+        <meta name='viewport' content='width=device-width, initial-scale=1'>
+        <link rel='stylesheet' type='text/css' media='screen' href='../estilos/login.css'>
+
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+        
+
+    </head>
+
+    <body style="margin: 0; padding: 0; background-color: #b1b7aa; height: 100vh;">
+
+        <?php
+            require_once('Caruosel_login.html');
+            require_once('TopBarL.html');
+            require_once('Shop.html');
+        require_once('footer.html');
+        ?>
+
+
+
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+    </body>
+
+</html>
